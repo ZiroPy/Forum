6754ea19041d0f347c679d9fa492a3917eb232ba
Javier Bocanegra <javi.bocaruiz09@gmail.com>
2023-10-24 11:37:45 +0200

    ReplyUpdate

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -19,7 +19,8 @@ class Forum extends Model
     // Funcion para señalar los datos que son rellenables
     protected $fillable = [
         'name',
-        'description'
+        'description',
+        'slug'
     ];
 
     public function posts(): HasMany{
